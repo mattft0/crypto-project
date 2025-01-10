05ab2c5ce992b3171a0391b500fc4c4a0d08d3f7
FewBerry <153908994+FewBerry@users.noreply.github.com>
2025-01-10 15:41:25 +0100

    Update ecdsa.py
    au propre sans test de vérification pas fonctionnel

--- ecdsa.py
+++ ecdsa.py
@@ -1,7 +1,8 @@
-from rfc7448 import x25519, add, computeVcoordinate, mult
+from rfc7748 import x25519, add, computeVcoordinate, mult
 from Crypto.Hash import SHA256
 from random import randint
 from algebra import mod_inv
+import secrets
 
 p = 2**255 - 19
 ORDER = (2**252 + 27742317777372353535851937790883648493)
@@ -14,16 +15,25 @@ def H(message):
     h = SHA256.new(message)
     return (int(h.hexdigest(), 16))
 
-def ECDSA_generate_nonce("""TBC"""):
-    return """TBC"""
+def ECDSA_generate_nonce():
+    return (int(secrets.randbelow(ORDER -1) +1))
+    
+def ECDSA_generate_keys(): #génère une clée privée qui est choisi aléatoirement dans ECDSA_generate_nonce | partie publique correspond à la "transposition" sur la courbe donc des coordonnées sur celle ci
+    priv = ECDSA_generate_nonce()
+    Pub_U, Pub_V = mult(priv, BaseU, BaseV, p)
+    return priv, (Pub_U, Pub_V)
+#print(ECDSA_generate_keys())
 
+def ECDSA_sign(message, private_key): # signature à partir de la clée privée et un message donnée 
+    hash_tempo = H(message)
+    nonce = ECDSA_generate_nonce()
+    U, _ = mult(nonce, BaseU, BaseV, p)
+    deriv = U % ORDER
 
-def ECDSA_generate_keys("""TBC"""):
-    return ("""TBC""")
-
-
-def ECDSA_sign("""TBC"""):
-    return ("""TBC""")    
+    k_inv = mod_inv(nonce, ORDER)
+    sign = (k_inv * (hash_tempo + deriv * private_key)) % ORDER
+           
+    return deriv, sign
 
 
 def ECDSA_verify("""TBC"""):
